58b12b3925f0e8dbfddd60028da764f6fec6cb2f
fengyimou <2977366691@qq.com>
2023-05-26 21:38:56 +0800

    用户功能的搜索部分页面的搭建

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,15 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import zhCn from 'element-plus/dist/locale/zh-cn.mjs'
+import { ref } from 'vue'
+const locale = ref(zhCn)
+</script>
 
 <template>
-  <div class="app">
-    <router-view></router-view>
-  </div>
+  <el-config-provider :locale="locale">
+    <div class="app">
+      <router-view></router-view>
+    </div>
+  </el-config-provider>
 </template>
 
 <style scoped>
--- src/views/system/user.vue
+++ src/views/system/user.vue
@@ -1,3 +1,21 @@
 <template>
-  <h2>用户</h2>
+  <div class="user">
+    <div class="user-search">
+      <user-search></user-search>
+    </div>
+    <div class="container">
+      <div class="table-title"></div>
+      <div class="table-main"></div>
+      <div class="table-pagination"></div>
+    </div>
+  </div>
 </template>
+<script setup lang="ts">
+import UserSearch from '@/components/system/user-search.vue'
+</script>
+<style lang="less" scoped>
+.user-search {
+  background-color: #fff;
+  border-radius: 8px 0;
+}
+</style>
